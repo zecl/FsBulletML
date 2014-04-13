122cc517d3557565b387fec7661835804d5b2cd7
zecl <zecl@hotmail.co.jp>
2014-04-13 16:12:37 +0900

    ドキュメント修正

---
+++ src/FsBulletML.Docs/releasenote.fsx
@@ -0,0 +1,23 @@
+﻿(**
+FsBulletML - ReleaseNote
+==================================
+
+0.8.4
+-------------
+- ``XML``形式の外部DSLをFsBulletML.Coreへ含めるよう修正。FsBulletML.Parserでは``SXML``形式、``FSB``形式のみ外部DSLを追加拡張する。
+
+0.8.3
+-------------
+- FsBulletML.Coreを.NET Framework3.5以上に対応
+- FsBulletML.Parserは.NET Framework4.0以上に対応
+
+0.8.2
+-------------
+- StructuredFormatDisplay属性によるBulletml判別共用体の文字列化対応。Bulletml.ToNodeString()メソッドで文字列を取得可。これにより``XML``形式等から、内部DSLのコードに変換可能に。
+
+0.8.1
+-------------
+- 最初のリリース
+
+
+*)
