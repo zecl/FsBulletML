7a306263e5a70c694ff1624ad688749d657edc0c
zecl <zecl@hotmail.co.jp>
2014-05-02 14:41:50 +0900

    0.8.8 release

--- src/FsBulletML.Docs/releasenote.fsx
+++ src/FsBulletML.Docs/releasenote.fsx
@@ -4,8 +4,10 @@ Release Notes
 ==================================
 0.8.8
 -------------
-- APIを変更(breaking change)。``BulletmlRunner.run``にて、弾の座標(X,Y)を直接変更しないようにし、移動分の値を返すよう変更（Unity考慮のため）。
-
+- ``Unity``からの利用を考慮してAPIと機能を変更(breaking change)
+    - ``BulletmlRunner.run``にて、弾の座標(X,Y)を直接変更しないようにし、移動分の値を返すよう変更。
+    - ``BulletmlRunner.run``にて、Tupleではなく``RunResult``型を返すよう変更。
+    - ``BulletmlInfo``型の``BulletmlTask``および``BulletmlTaskOption``をメンバへ変更。
 
 0.8.7
 -------------
